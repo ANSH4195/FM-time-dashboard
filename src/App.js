401fd998f63dd96e946e17a8f94565b78b637838
Ansh Shrivastava <shri.ansh4563@gmail,com>
2022-02-10 12:21:11 +0530

    User Box Styling complete

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App container'>
+      <div className='grid'>
+        <div className='info-card'>
+          <div className='info-card-top'>
+            <img
+              src='images/image-jeremy.png'
+              alt='Jeremy'
+              className='avatar'
+            />
+            <div className='avatar-description'>
+              <small className='pre-title'>Report for</small>
+              <h3 className='avatar-name'>Jeremy Robson</h3>
+            </div>
+          </div>
+          <div className='info-card-bottom'>
+            <ul>
+              <li>
+                <button className='btn-link'>Daily</button>
+              </li>
+              <li>
+                <button className='btn-link active'>Weekly</button>
+              </li>
+              <li>
+                <button className='btn-link'>Monthly</button>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
